db63b8daae131541e5ccaffedf045075bf361e7c
mairess <trabalho.maires@gmail.com>
2023-04-09 18:15:20 -0300

    querySelectorAll has been learned

---
+++ selecionando-elementos/querySelectorAll.js
@@ -0,0 +1,9 @@
+const elements = document.querySelectorAll('#one');
+
+
+
+
+elements.forEach(el => console.log(el))
+
+// const element = document.querySelectorAll('#one');
+// console.log(element)
