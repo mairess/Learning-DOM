96fef5695aa6ce397c6ad5e8fd843e1f8c5dc71d
mairess <trabalho.maires@gmail.com>
2023-04-09 18:04:26 -0300

    querySelector has been learned

---
+++ selecionando-elementos/querySelector.js
@@ -0,0 +1,10 @@
+// const element = document.querySelector('.one');
+// console.log(element)
+
+// const element = document.querySelector('meta');
+// console.log(element)
+
+
+const element = document.querySelector('[src]');
+console.log(element)
+
