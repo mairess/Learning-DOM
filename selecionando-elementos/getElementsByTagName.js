7610ec59cd9548fb7313938f9fc2cb3ad2baa2f2
mairess <trabalho.maires@gmail.com>
2023-04-09 17:35:48 -0300

    getElementsByTagName has been learned

---
+++ selecionando-elementos/getElementsByTagName.js
@@ -0,0 +1,10 @@
+// const element = document.getElementsByTagName('meta');
+// console.log(element)
+
+
+// const element = document.getElementsByTagName('head');
+// console.log(element)
+
+const element = document.getElementsByTagName('h1');
+console.log(element)
+
