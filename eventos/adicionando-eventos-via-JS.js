b144b5aef413a44c3ebeaf9758e0a4e4afa04a40
mairess <trabalho.maires@gmail.com>
2023-04-10 12:12:23 -0300

    adicionando eventos via JS has been learned

---
+++ eventos/adicionando-eventos-via-JS.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector('h1')
+
+// h1.addEventListener('click', print)
+
+// function print() {
+//     console.log('print')
+// }
+
+h1.addEventListener('mouseout', print)
+
+function print() {
+    console.log('print')
+}
