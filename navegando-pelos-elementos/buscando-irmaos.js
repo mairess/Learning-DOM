ef83fb313289a78cd84c982583d958c55a6bcc82
mairess <trabalho.maires@gmail.com>
2023-04-10 11:18:30 -0300

    buscando irmãos has been learned

---
+++ navegando-pelos-elementos/buscando-irmaos.js
@@ -0,0 +1,6 @@
+const el = document.querySelector('body script')
+
+// console.log(el.nextSibling)
+// console.log(el.nextElementSibling)
+// console.log(el.previousSibling)
+console.log(el.previousElementSibling)
