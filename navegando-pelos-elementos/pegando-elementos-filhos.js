ccf5960871b55fdd480962ac84b4e4b84bb625d8
mairess <trabalho.maires@gmail.com>
2023-04-10 11:07:31 -0300

    pegando elementos filhos has been learned

---
+++ navegando-pelos-elementos/pegando-elementos-filhos.js
@@ -0,0 +1,7 @@
+const el = document.querySelector('body')
+
+// console.log(el.firstChild)
+//console.log(el.firstElementChild)
+// console.log(el.children)
+console.log(el.lastChild)
+// console.log(el.lastElementChild)
