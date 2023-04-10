3cb0def204751a14c13ac7df34ad1cf553658792
mairess <trabalho.maires@gmail.com>
2023-04-10 10:43:58 -0300

    classList has benn learned

---
+++ manipulando-estilos-e-classes/classList.js
@@ -0,0 +1,8 @@
+const element = document.querySelector('body')
+
+element.classList.add('active', 'green')
+
+console.log(element.classList)
+
+// element.classList.remove('active')
+element.classList.toggle('active')
