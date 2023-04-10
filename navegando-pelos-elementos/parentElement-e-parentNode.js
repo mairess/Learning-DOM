b766bd85505941bdfdd196b1d194acf766bed0b9
mairess <trabalho.maires@gmail.com>
2023-04-10 10:54:34 -0300

    parentElement-parentNode has been learned

---
+++ navegando-pelos-elementos/parentElement-e-parentNode.js
@@ -0,0 +1,11 @@
+const body = document.querySelector('body')
+
+console.log(body.parentNode)
+
+console.log(body.parentElement)
+
+// const element = document.querySelector('h1')
+
+// console.log(element.parentNode)
+
+// console.log(element.parentElement)
